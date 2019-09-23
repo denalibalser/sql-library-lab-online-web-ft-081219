111206422bdce81647a71b6f9e44596ee25cd4d7
Denali Balser <denali.balser167@gmail.com>
2019-09-23 13:27:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM characters
   INNER JOIN series
-  ON series.id = characters.series_id
+  ON characters.series_id = series.id
   GROUP BY series.title, species
   HAVING characters.species = 'human'
   ORDER BY COUNT(species) DESC LIMIT 1;"
